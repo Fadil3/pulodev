c9a33a8c9134e2c5808f8fa72c5b7f513c16f15c
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-01-22 07:30:04 +0700

    add navbar to home

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
+import Navbar from '../components/Navbar'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod,
-        nisl eget consectetur consectetur, nisi nisl tincidunt nisi, eu aliquet
-        nisl nunc eu nisl. Nullam eget nisl eu nisi porta consectetur. Donec
-        euismod, nisl eget consectetur consectetur, nisi nisl tincidunt nisi, eu
-        aliquet nisl nunc eu nisl.
-      </p>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
